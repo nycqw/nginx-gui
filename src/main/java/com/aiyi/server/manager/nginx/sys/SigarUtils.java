82e10c9dbef08a2978641f5c31c0708f9d4e5702
nycqw <196612cqw>
2019-06-20 15:30:26 +0800

    nginx conf modify

--- src/main/java/com/aiyi/server/manager/nginx/sys/SigarUtils.java
+++ src/main/java/com/aiyi/server/manager/nginx/sys/SigarUtils.java
@@ -5,15 +5,23 @@ import org.hyperic.sigar.Sigar;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 @Slf4j
 public class SigarUtils {
     public final static Sigar sigar = initSigar();
 
     private static Sigar initSigar() {
+        String osName = System.getProperty("os.name");
         // 寻找 classpath 根目录下的 sigar 文件夹
         String userDir = System.getProperty("user.dir");
-        File classPath = new File(userDir + "/sigarLibs");
+        File classPath;
+        if ("Windows".equals(osName)) {
+            classPath = new File(userDir + "/sigarLibs");
+        } else {
+            classPath = new File(userDir + "/classes/sigarLibs");
+        }
+
         try {
             // 追加库路径
             String sigarLibsPath = classPath.getCanonicalPath();
